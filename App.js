885c8a0a71da0fecfdc5b681d762dcafa1e88ee8
EugeneKramer <eugenekramerp@outlook.com>
2016-06-20 09:17:34 -0400

    1-6

--- App.js
+++ App.js
@@ -2,45 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 class App extends React.Component {
-
     constructor(){
         super();
-        this.state={
-            txt:"this is a state",
-            cat:0
-        }
+        this.state={txt:""}
+        this.update = this.update.bind(this);
     }
     update(e){
         this.setState({txt:e.target.value});
     }
 
     render(){
-        //let txt=this.props.txt;
         return (
             <div>
-                <input type="text"
-                       onChange={this.update.bind(this)}/>
-                <h1>{this.state.txt}</h1>
+                <Widget txt={this.state.txt}
+                        update={this.update} />
+                <Widget txt={this.state.txt}
+                        update={this.update} />
+                <Widget txt={this.state.txt}
+                        update={this.update} />
+                <Widget txt={this.state.txt}
+                        update={this.update} />
             </div>
         );
     }
 }
+$(function() {
+    ReactDOM.render(
+        <App txt="stuff"/>,
+        document.getElementById('app')
+    )
+});
 
-App.propTypes={
-    txt:React.PropTypes.string,
-    cat:React.PropTypes.number.isRequired
+const Widget = (props) => {
+    "use strict";
+    return (
+        <div>
+            <input type="text"
+                   onChange={props.update}/>
+            <h1>{props.txt}</h1>
+        </div>
+    )
 }
-
-App.defaultProps={
-    txt:'this is the default txt',
-    cat:5
-}
-ReactDOM.render(
-    <App txt="stuff"/>,
-    document.getElementById('app')
-)
-
-
 
 const Thing2 = () => <div><h1>Hello</h1></div>
 export default App;
