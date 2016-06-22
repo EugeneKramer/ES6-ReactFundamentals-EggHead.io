83aaf0c14b0826bff70f9c0b5318382013c0925d
EugeneKramer <eugenekramerp@outlook.com>
2016-06-22 17:52:35 -0400

    1-11

--- App.js
+++ App.js
@@ -2,32 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
-
 class App extends React.Component {
     constructor(){
         super();
         this.update=this.update.bind(this);
-        this.state={increasing:false}
+        this.state = {increasing:0};
         console.log("constructing");
     }
     update(){
+        console.log("update");
+        console.log(this.props.val);
+
         ReactDOM.render(
             <App val={this.props.val+1}/>,
             document.getElementById('app')
         );
     }
-    componentWllReceiveProps(nextProps){
-        this.setState({increasing: nextProps.val > this.props.val})
-    }
-    shouldComponenUpdate(nextProps, nextState){
-        return nextProps.val % 5 === 0;
-    }
-    componentDidUpdate(prevProps, prevState){
-        console.log('prevprops',prevProps)
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        this.setState({increasing:nextProps.val > this.props.val})
     }
 
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.val % 5 === 0;
+    }
+
+
     render() {
-        console.log("rendering");
+        console.log("rrefnderijng");
         console.log(this.state.increasing);
         return (
             <button onClick={this.update}>
@@ -40,10 +42,10 @@ class App extends React.Component {
 App.defaultProps = {val:0}
 
 $(function() {
-    //ReactDOM.render(
-    //    <App txt="stuff"/>,
-    //    document.getElementById('app')
-    //)
+    ReactDOM.render(
+        <App txt="stuff"/>,
+        document.getElementById('app')
+    )
 });
 
 const Thing2 = () => <div><h1>Hello</h1></div>
